75b753580863c7c43ae49b8aab85ad7a59e06caf
Ivan Cavalheiro <ivan.cavalheiro@hogarth.com>
2023-11-27 15:34:05 -0300

    Added support for multiple recipients

--- mail.js
+++ mail.js
@@ -2,8 +2,12 @@ const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 function sendMail (text, serviceName, callback) {
+    let to = process.env.SEND_TO_EMAILS;
+    if (to.includes(';')) {
+        to = to.split(';');
+    }
     const msg = {
-        to: process.env.SEND_TO_EMAILS,
+        to,
         from: process.env.FROM_EMAIL,
         subject: serviceName + " | " + process.env.EMAIL_SUBJECT,
         text,
